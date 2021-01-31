3456c1bda3b8bc9cf2f97f30a2489e533dc6e747
Elnur01 <30310749+Elnur01@users.noreply.github.com>
2021-01-31 13:34:34 +0400

    Create printing.py

---
+++ printing.py
@@ -0,0 +1,15 @@
+#for printing even numbers
+num = [2,5,7,8,54,34,90,13,17]
+
+for x in num:
+    if x%2==0:
+        print(x)
+        
+       
+#for printing odd numbers
+
+num = [2,5,7,8,54,34,90,13,17]
+
+for x in num:
+    if x%2:
+        print(x)
